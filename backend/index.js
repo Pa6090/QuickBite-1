9d8ab639d72b1a479623a7e3cec949a115d5ef6a
Pa6090 <pavanmittapalli1@gmail.com>
2024-05-04 11:12:56 +0530

    Added front end site in cors backend

--- backend/index.js
+++ backend/index.js
@@ -14,19 +14,19 @@ const port = process.env.PORT;
 const app = express()
 
 app.use(cors({
-            origin : ["http://localhost:3000/"],
+            origin : ["https://quickbite-1-8mfa.onrender.com/"],
             methods : ["POST", "GET"],
             credentials : true
         })
 )
 
-app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+// app.use((req,res,next)=>{
+//     res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+//     res.header(
+//         "Access-Control-Allow-Headers",
+//         "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
 
 app.use(express.json())
 app.use(cookieParser())
